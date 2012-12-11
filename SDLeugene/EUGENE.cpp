24e2e33e637cc6b037756c1a07c8b81a6975db20
Eugene <ultamiteshowdown@hotmail.com>
2012-12-10 22:42:27 -0500

    worked on SDL lesson 2

--- SDLeugene/EUGENE.cpp
+++ SDLeugene/EUGENE.cpp
@@ -14,8 +14,41 @@ SDL_Surface *background = NULL;
 SDL_Surface *screen = NULL;
 
 
-int main(){
+int main(int argc,char *args[]){
+	if (SDL_Init(SDL_INIT_EVERYTHING)==-1){
+		return 1;
+	}
+	//set up the screen
+    screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
+    if (screen==NULL){
+		return 1;
+    }
+    //set window caption
+	SDL_WM_SetCaption("Shutengu",NULL);
+	//load images
+	message=load_image("cb.bmp");
+	background=load_image("bg.bmp");
+	//apply background to surface
+	apply_surface(0,0,background,screen);
+	//Apply the message to the screen
+    apply_surface( 180, 140, message, screen );
+    //Update the screen
+    if( SDL_Flip( screen ) == -1 )
+    {
+    	printf("no update");
+        return 1;
+    }
+	//Wait 2 seconds
+    SDL_Delay( 2000 );
+    //Free the surfaces
+    SDL_FreeSurface( message );
+    SDL_FreeSurface( background );
 
+    //Quit SDL
+    SDL_Quit();
+
+    //Return
+    return 0;
 }
 SDL_Surface *load_image(char *filename){
 	//Temporary storage for the image that's loaded
@@ -31,5 +64,13 @@ SDL_Surface *load_image(char *filename){
     return optimizedImage;
 }
 void apply_surface(int x,int y,SDL_Surface *source, SDL_Surface *destination){
+	//temp rectangle to hold offsets
+	SDL_Rect offset;
+	//give offsets to rectangle
+	offset.x=x;
+	offset.y=y;
+
+	//blit the surface
+	SDL_BlitSurface( source, NULL, destination, &offset );
 
 }
